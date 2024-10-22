5bf62750cf5a243f6d5314fa88c7993dc899071b
Egor <Egor@DESKTOP-QNQE2EQ>
2024-10-22 23:27:39 +0700

    Изменена функция ToObj, удалена проверка на ковычки в вводных данных и удалены ненужные элементы.

--- Peace/Program.cs
+++ Peace/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Peace
 {
@@ -11,31 +12,21 @@ namespace Peace
     {
         static void Main(string[] args)
         {
-            string test = "17.02.2022             20:30:48                \"Джордж оавлыд\"";
+            string test = "17.02.2022             20:30:48                \"Джордж Хендрикс\"";
             Subject ToObj(string str)
             {
-                //
                 str = str.Trim();
                 List<string> items = str.Split('"').ToList();
-                if (items.Count == 3) 
-                {
-                    List<string> items2 = items[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-                    string name = items[1].Trim();
-                    DateTime date = DateTime.Parse(items2[0].Trim()).Date;
-                    TimeSpan time = DateTime.Parse(items2[1].Trim()).TimeOfDay;
-                    return new Subject(date, time, name);
-                }
-                else
-                {
-                    List<string> items2 = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
-                    string name = items2[2].Trim();
-                    DateTime date = DateTime.Parse(items2[0].Trim()).Date;
-                    TimeSpan time = DateTime.Parse(items2[1].Trim()).TimeOfDay;
-                    return new Subject(date, time, name);
-                }
+                string name = "";
+                List<string> values = new List<string>();
+                values = items[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                name = items[1].Trim();
+                DateTime date = DateTime.Parse(values[0].Trim()).Date;
+                TimeSpan time = DateTime.Parse(values[1].Trim()).TimeOfDay;
+                return new Subject(date, time, name);
             }
             Subject testObj = ToObj(test);
-            Console.WriteLine($"Дата: {testObj.Date.ToString("dd.MM.yyyy")}\nВремя: {testObj.Time}\nИмя:{testObj.Name}");
+            Console.WriteLine($"Дата: {testObj.Date.ToString("dd.MM.yyyy")}\nВремя: {testObj.Time}\nИмя: {testObj.Name}");
             Console.ReadKey();
         }
     }
