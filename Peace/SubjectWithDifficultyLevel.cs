599317e8eda4f29e010975ce24a9c6b38177f752
Egor <Egor@DESKTOP-QNQE2EQ>
2024-10-22 23:16:30 +0700

    Добавлено 2 новых класса, наследуемых от изначального класса Subject

---
+++ Peace/SubjectWithDifficultyLevel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peace
+{
+    internal class SubjectWithDifficultyLevel : Subject
+    {
+        public int DifficultyLevel { get; set; }
+        public SubjectWithDifficultyLevel(DateTime date, TimeSpan time, string name, int difficultyLevel):base(date,time,name)
+        {
+            Date = date;
+            Time = time;
+            Name = name;
+            DifficultyLevel = difficultyLevel;
+        }
+    }
+}
---
+++ Peace/SubjectWithType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peace
+{
+    internal class SubjectWithType : Subject
+    {
+        public string TypeOfSubject { get; set; }
+        public SubjectWithType(string typeOfSubject, DateTime date, TimeSpan time, string name) : base(date, time, name)
+        {
+            TypeOfSubject = typeOfSubject;
+        }
+    }
+}
